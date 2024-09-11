9d517ca80665b48a679a7e408728b10a722d2983
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-09-11 23:19:26 +0900

    add glocal_eval

--- ad_target_path_planner/include/ad_target_path_planner/ad_target_path_planner.h
+++ ad_target_path_planner/include/ad_target_path_planner/ad_target_path_planner.h
@@ -16,93 +16,104 @@
 // ===== 構造体 =====
 struct State
 {
-    double x;    // [m]
-    double y;    // [m]
-    double yaw;  // [rad]
+  double x;  // [m]
+  double y;  // [m]
+  double yaw; // [rad]
 };
 
 // ===== クラス =====
 class AdTargetPathPlanner
 {
 public:
-    AdTargetPathPlanner();
-    void process();
+  AdTargetPathPlanner();
+  void process();
 
 private:
-    // コールバック関数
-    void cost_map_callback(const nav_msgs::OccupancyGridConstPtr& msg);
-    void local_goal_callback(const geometry_msgs::PointStampedConstPtr& msg);
+  // コールバック関数
+  void cost_map_callback(const nav_msgs::OccupancyGridConstPtr& msg);
+  void local_goal_callback(const geometry_msgs::PointStampedConstPtr& msg);
+  void glocal_path_callback(const nav_msgs::PathConstPtr& msg);
 
-    // 引数あり関数
-    bool is_goal_check(const double x, const double y);                                  // pathの終端がlocal_goalから一定距離内に到達したか確認
-    bool is_finish_check(const double x, const double y);                                // pathの終端がロボットから一定距離離れたか確認
-    double calc_evaluation(const std::vector<State>& traj);                              // 評価関数を計算する
-    double calc_heading_eval(const std::vector<State>& traj);                            // heading（1項目）の評価関数を計算する
-    double calc_cost_map_eval(const std::vector<State>& traj);                           // cost_map（2項目）の評価関数を計算する
-    double normalize_angle(double theta);                                                // 適切な角度(-M_PI ~ M_PI)を返す
-    int xy_to_grid_index(const double x, const double y);                                // 座標からグリッドのインデックスを返す
-    void search_node(const double max_rad, std::vector<State>& nodes);                   // 次のノードを探索
-    void create_path(const double max_rad);                                              // 目標軌道を生成
-    void transform_node_to_path(const std::vector<State>& nodes, nav_msgs::Path& path);  // ノード情報からパスを生成
+  // 引数あり関数
+  bool is_goal_check(const double x, const double y);                                  // pathの終端がlocal_goalから一定距離内に到達したか確認
+  bool is_finish_check(const double x, const double y);                                // pathの終端がロボットから一定距離離れたか確認
+  double calc_evaluation(const std::vector<State>& traj);                              // 評価関数を計算する
+  double calc_heading_eval(const std::vector<State>& traj);                            // heading（1項目）の評価関数を計算する
+  double calc_glocal_heading_eval(const std::vector<State>& traj);                     // glocal_heading（2項目）の評価関数を計算する
+  double calc_cost_map_eval(const std::vector<State>& traj);                           // cost_map（3項目）の評価関数を計算する
+  double normalize_angle(double theta);                                                // 適切な角度(-M_PI ~ M_PI)を返す
+  int xy_to_grid_index(const double x, const double y);                                // 座標からグリッドのインデックスを返す
+  void search_node(const double max_rad, std::vector<State>& nodes);                   // 次のノードを探索
+  void create_path(const double max_rad);                                              // 目標軌道を生成
+  void transform_node_to_path(const std::vector<State>& nodes, nav_msgs::Path& path);  // ノード情報からパスを生成
 
 
-    // 引数なし関数
-    double calc_rad_with_steer();  // 機構的制約内で旋回可能な角度を計算（ステアあり）
-    double calc_rad_no_steer();    // 機構的制約内で旋回可能な角度を計算(ステアなし)
+  // 引数なし関数
+  double calc_rad_with_steer();       // 機構的制約内で旋回可能な角度を計算(ステアあり)
+  double calc_rad_no_steer();         // 機構的制約内で旋回可能な角度を計算(ステアなし)
+  double calc_dist_to_glocal_goal();  // glocal_goalまでの距離を計算
+  void update_glocal_goal();          // glocal_goalを更新
 
-    // yamlファイルで設定可能な変数
-    int hz_;                  // ループ周波数 [Hz]
-    std::string node_frame_;  // 生成するノードのframe_id
-    std::string path_frame_;  // 生成するpathのframe_id
-    std::string goal_frame_;  // local_goalのframe_id
-    double goal_tolerance_;   // local_goal_に対する許容誤差 [m]
-    double finish_dist_;      // ロボットからノード先端までの距離 [m]
-    double max_vel_;          // 最高並進速度 [m/s]
-    double max_yawrate_;      // 最高旋回速度 [rad/s]
-    double max_speed_;        // タイヤの最高回転速度 [m/s]
-    double speed_reso_;       // 速度を探索する際の刻み幅 [m/s]
-    double max_steer_angle_;  // ステア角の最大値 [deg]
-    double steer_angle_reso_;  // ステア角を探索するときの刻み幅 [deg]
-    double tread_;            // ccvのトレッド [m]
-    double path_reso_;        // 生成するpathの刻み幅 [m]
-    double theta_reso_;       // 候補となるpathを生成する際の方位の刻み幅 [rad]
-    int search_step_;         // 何ステップ先まで評価値を計算するか
-    double weight_heading_;   // 評価関数1項目　重みづけ定数
-    double weight_cost_map_;  // 評価関数2項目　重みづけ定数
-    double min_cost_;         // 割り当てるコストの最小値
+  // yamlファイルで設定可能な変数
+  int hz_;                   // ループ周波数 [Hz]
+  std::string node_frame_;   // 生成するノードのframe_id
+  std::string path_frame_;   // 生成するpathのframe_id
+  std::string goal_frame_;   // local_goalのframe_id
+  double goal_tolerance_;    // local_goal_に対する許容誤差 [m]
+  double finish_dist_;       // ロボットからノード先端までの距離 [m]
+  double dist_to_update_glocal_goal_;  // glocal_goalを更新する距離 [m]
+  double max_vel_;           // 最高並進速度 [m/s]
+  double max_yawrate_;       // 最高旋回速度 [rad/s]
+  double max_speed_;         // タイヤの最高回転速度 [m/s]
+  double speed_reso_;        // 速度を探索する際の刻み幅 [m/s]
+  double max_steer_angle_;   // ステア角の最大値 [deg]
+  double steer_angle_reso_;  // ステア角を探索するときの刻み幅 [deg]
+  double tread_;             // ccvのトレッド [m]
+  double path_reso_;         // 生成するpathの刻み幅 [m]
+  double theta_reso_;        // 候補となるpathを生成する際の方位の刻み幅 [rad]
+  int search_step_;          // 何ステップ先まで評価値を計算するか
+  double weight_heading_;    // 評価関数1項目　重みづけ定数
+  double weight_glocal_heading_;  // 評価関数2項目　重みづけ定数
+  double weight_cost_map_;   // 評価関数2項目　重みづけ定数
+  double min_cost_;          // 割り当てるコストの最小値
 
-    // その他の変数
-    double tmp_x_;    // 1ステップ前のロボットのx座標格納用
-    double tmp_y_;    // 1ステップ前のロボットのy座標格納用
-    double tmp_yaw_;  // 1ステップ前のロボットのyaw角格納用
+  // その他の変数
+  double tmp_x_;      // 1ステップ前のロボットのx座標格納用
+  double tmp_y_;      // 1ステップ前のロボットのy座標格納用
+  double tmp_yaw_;     // 1ステップ前のロボットのyaw角格納用
+  int glocal_path_index_; // glocal_pathのインデックス
 
-    // msgの受け取り判定用
-    bool flag_cost_map_ = false;
-    bool flag_local_goal_ = false;
+  // msgの受け取り判定用
+  bool flag_cost_map_ = false;
+  bool flag_local_goal_ = false;
+  bool flag_glocal_path_ = false;
 
-    // local_goalから一定距離内に到達したかの確認用
-    bool flag_goal_check_ = false;
+  // local_goalから一定距離内に到達したかの確認用
+  bool flag_goal_check_ = false;
 
-    // 座標変換の判定用
-    bool flag_frame_change_ = false;
+  // 座標変換の判定用
+  bool flag_frame_change_ = false;
 
-    // NodeHandle
-    ros::NodeHandle nh_;
-    ros::NodeHandle private_nh_;
+  // NodeHandle
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
 
-    // Subscriber
-    ros::Subscriber sub_cost_map_;
-    ros::Subscriber sub_local_goal_;
+  // Subscriber
+  ros::Subscriber sub_cost_map_;
+  ros::Subscriber sub_local_goal_;
+  ros::Subscriber sub_glocal_path_;
 
-    // Publisher
-    ros::Publisher pub_target_path_;
+  // Publisher
+  ros::Publisher pub_target_path_;
 
-    // tf
-    tf2_ros::Buffer tf_buffer_;
+  // tf
+  tf2_ros::Buffer tf_buffer_;
 
-    // 各種オブジェクト
-    nav_msgs::OccupancyGrid cost_map_;        // コストマップ
-    geometry_msgs::PointStamped local_goal_;  // local_goal
+  // 各種オブジェクト
+  nav_msgs::OccupancyGrid cost_map_;         // コストマップ
+  geometry_msgs::PointStamped local_goal_;   // local_goal
+  nav_msgs::Path glocal_path_;               // glocal_path
+  geometry_msgs::PointStamped glocal_goal_;  // glocal_goal
 };
 
 #endif // AD_TARGET_PATH_PLANNER_H
--- ad_target_path_planner/src/ad_target_path_planner.cpp
+++ ad_target_path_planner/src/ad_target_path_planner.cpp
@@ -6,28 +6,32 @@ AdTargetPathPlanner::AdTargetPathPlanner():private_nh_("~")
   private_nh_.param("node_frame", node_frame_, {"base_footprint"});
   private_nh_.param("path_frame", path_frame_, {"odom"});
   private_nh_.param("goal_frame", goal_frame_, {"odom"});
-  private_nh_.param("goal_tolerance", goal_tolerance_, {0.5});
-  private_nh_.param("finish_dist", finish_dist_, {4.9});
+  private_nh_.param("goal_tolerance", goal_tolerance_, {1.0});
+  private_nh_.param("finish_dist", finish_dist_, {2.0});
+  private_nh_.param("dist_to_update_glocal_goal", dist_to_update_glocal_goal_, {1.1});
   private_nh_.param("max_vel", max_vel_, {1.2});
-  private_nh_.param("max_yawrate", max_yawrate_, {1.0});
+  private_nh_.param("max_yawrate", max_yawrate_, {4.5});
   private_nh_.param("max_speed", max_speed_, {3.0});
   private_nh_.param("speed_reso", speed_reso_, {0.1});
   private_nh_.param("max_steer_angle", max_steer_angle_, {20});
   private_nh_.param("steer_angle_reso", steer_angle_reso_, {1.0});
   private_nh_.param("tread", tread_, {0.5});
-  private_nh_.param("path_reso", path_reso_, {0.05});
+  private_nh_.param("path_reso", path_reso_, {0.1});
   private_nh_.param("theta_reso", theta_reso_, {0.01});
   private_nh_.param("search_step", search_step_, {10});
   private_nh_.param("weight_heading", weight_heading_, {0.1});
+  private_nh_.param("weight_glocal_heading", weight_glocal_heading_, {0.1});
   private_nh_.param("weight_cost_map", weight_cost_map_, {0.9});
-  private_nh_.param("min_cost", min_cost_, {10.0});
+  private_nh_.param("min_cost", min_cost_, {30.0});
   private_nh_.param("tmp_x", tmp_x_, {0.0});
   private_nh_.param("tmp_y", tmp_y_, {0.0});
   private_nh_.param("tmp_yaw", tmp_yaw_, {0.0});
+  private_nh_.param("glocal_path_index", glocal_path_index_, {0});
 
   //Subscriber
   sub_cost_map_ = nh_.subscribe("/cost_map", 1, &AdTargetPathPlanner::cost_map_callback, this, ros::TransportHints().reliable().tcpNoDelay());
   sub_local_goal_ = nh_.subscribe("/local_goal", 1, &AdTargetPathPlanner::local_goal_callback, this, ros::TransportHints().reliable().tcpNoDelay());
+  sub_glocal_path_ = nh_.subscribe("/glocal_path", 1, &AdTargetPathPlanner::glocal_path_callback, this, ros::TransportHints().reliable().tcpNoDelay());
 
   //Publisher
   pub_target_path_ = nh_.advertise<nav_msgs::Path>("/target_path", 1);
@@ -60,7 +64,16 @@ void AdTargetPathPlanner::local_goal_callback(const geometry_msgs::PointStampedC
   tf2::doTransform(*msg, local_goal_, transform);
 }
 
-// 機構的制約内で旋回可能な角度を計算
+// glocal_pathのコールバック関数
+void AdTargetPathPlanner::glocal_path_callback(const nav_msgs::PathConstPtr& msg)
+{
+  glocal_path_ = *msg;
+  
+  if(glocal_path_.poses.size() > 0)
+    flag_glocal_path_ = true;
+}
+
+// 機構的制約内で旋回可能な角度を計算(ステアあり)
 double AdTargetPathPlanner::calc_rad_with_steer()
 {
   double max_omega = 0.0;
@@ -99,8 +112,6 @@ double AdTargetPathPlanner::calc_rad_with_steer()
           {
             max_omega = omega;
             ROS_INFO_STREAM(omega << " " << steer_angle << " " << v_r << " " << v_l << " " << v << " " << r_l << " " << r_r);
-
-
           }
         }
       }
@@ -115,6 +126,7 @@ double AdTargetPathPlanner::calc_rad_with_steer()
 
 }
 
+// 機構的制約内で旋回可能な角度を計算(ステアなし)
 double AdTargetPathPlanner::calc_rad_no_steer()
 {
   // 旋回で変わる方位の最大値を計算
@@ -151,13 +163,47 @@ bool AdTargetPathPlanner::is_finish_check(const double x, const double y)
     return true;
 }
 
+// glocal_goalまでの距離を計算
+double AdTargetPathPlanner::calc_dist_to_glocal_goal()
+{
+  double dx = glocal_path_.poses[glocal_path_index_].pose.position.x - tmp_x_;
+  double dy = glocal_path_.poses[glocal_path_index_].pose.position.y - tmp_y_;
+
+  return hypot(dx, dy);
+}
+
+// glocal_goalの更新
+void AdTargetPathPlanner::update_glocal_goal()
+{
+  double dist_to_local_goal = calc_dist_to_glocal_goal();
+
+  while(dist_to_local_goal < dist_to_update_glocal_goal_)
+  {
+    glocal_path_index_++;
+    dist_to_local_goal = calc_dist_to_glocal_goal();
+
+    // glocal_path_indexがglocal_path_index_.posesの配列の要素数を超えたら、glocal_goalとしてglocal_pathのゴールを設定
+    if(glocal_path_index_ >= glocal_path_.poses.size())
+    {
+      glocal_path_index_ = glocal_path_.poses.size() - 1;
+      break;
+    }
+  }
+
+  // glocal_goalの更新
+  glocal_goal_.header.frame_id = node_frame_;
+  glocal_goal_.point.x = glocal_path_.poses[glocal_path_index_].pose.position.x;
+  glocal_goal_.point.y = glocal_path_.poses[glocal_path_index_].pose.position.y;
+}
+
 // 評価関数を計算する
 double AdTargetPathPlanner::calc_evaluation(const std::vector<State>& traj)
 {
   double heading_value = weight_heading_ * calc_heading_eval(traj);
+  double glocal_heading_value = weight_glocal_heading_ * calc_glocal_heading_eval(traj);
   double cost_map_value = weight_cost_map_ * calc_cost_map_eval(traj);
 
-  double total_score = heading_value - cost_map_value;
+  double total_score = heading_value + glocal_heading_value - cost_map_value;
 
   return total_score;
 }
@@ -180,16 +226,39 @@ double AdTargetPathPlanner::calc_heading_eval(const std::vector<State>& traj)
     target_theta = theta -goal_theta;
 
   //headingの評価値を計算
-  double heading_eval = (M_PI - abs(normalize_angle(target_theta))) / M_PI; //正規化
+  double heading_eval = (M_PI - abs(normalize_angle(target_theta))) / M_PI;  //正規化
 
   return heading_eval;
 }
 
-// cost_map（2項目）の評価関数を計算する
+// glocal_heading（2項目）の評価関数を計算する
+// 大きいほど良い
+double AdTargetPathPlanner::calc_glocal_heading_eval(const std::vector<State>& traj)
+{
+  //最終時刻でのロボットの方向
+  double theta = traj.back().yaw;
+
+  //最終時刻での位置に対するゴール方向
+  double goal_theta = atan2(glocal_goal_.point.y - traj.back().y, glocal_goal_.point.x - traj.back().x);
+
+  //ゴールまでの方位差分
+  double target_theta = 0.0; //初期化
+  if(goal_theta > theta)
+    target_theta = goal_theta - theta;
+  else if(goal_theta < theta)
+    target_theta = theta -goal_theta;
+
+  //headingの評価値を計算
+  double glocal_heading_eval = (M_PI - abs(normalize_angle(target_theta))) / M_PI;  //正規化
+
+  return glocal_heading_eval;
+}
+
+// cost_map（3項目）の評価関数を計算する
 // 大きいほど良くない
 double AdTargetPathPlanner::calc_cost_map_eval(const std::vector<State>& traj)
 {
-  double total_cost = 0; // 通過した場所の走行コストの合計値
+  double total_cost = 0;  // 通過した場所の走行コストの合計値
 
   for(auto& state : traj)
   {
@@ -201,7 +270,7 @@ double AdTargetPathPlanner::calc_cost_map_eval(const std::vector<State>& traj)
 
   int traj_size = traj.size();
   if(traj_size == 0)
-    traj_size = 1; // ゼロ割り防止
+    traj_size = 1;  // ゼロ割り防止
   
   double cost_map_eval = total_cost / (min_cost_ * traj_size);
 
@@ -236,12 +305,15 @@ void AdTargetPathPlanner::search_node(const double max_rad, std::vector<State>& 
   double opt_y = 0.0;
   double opt_yaw = 0.0;
 
-  double max_score = -1000.0; //評価値の最大値格納用
+  // glocal_goalの更新
+  update_glocal_goal();
+
+  double max_score = -1000.0;  //評価値の最大値格納用
 
   for(double theta=-max_rad; theta<=max_rad;theta+=theta_reso_)
   {
-    std::vector<State> traj; // 軌跡格納用
-    State state = {0.0, 0.0, 0.0}; // 軌跡生成用の仮想ロボット
+    std::vector<State> traj;        // 軌跡格納用
+    State state = {0.0, 0.0, 0.0};  // 軌跡生成用の仮想ロボット
 
     // yaw角を計算
     double yaw = tmp_yaw_ + theta;
@@ -286,7 +358,7 @@ void AdTargetPathPlanner::create_path(const double max_rad)
   path.header.stamp = ros::Time::now();
   path.header.frame_id = path_frame_;
 
-  std::vector<State> nodes; // ノード情報格納用
+  std::vector<State> nodes;  // ノード情報格納用
 
   // 初期値を代入
   State node = {0.0, 0.0, 0.0};
@@ -296,6 +368,7 @@ void AdTargetPathPlanner::create_path(const double max_rad)
   tmp_x_ = 0.0;
   tmp_y_ = 0.0;
   tmp_yaw_ = 0.0;
+  glocal_path_index_ = 0;
   flag_goal_check_ = false;
 
   int step_counter = 0;
@@ -305,7 +378,7 @@ void AdTargetPathPlanner::create_path(const double max_rad)
   {
     search_node(max_rad, nodes);
     step_counter++;
-
+    
     // 終了判定
     if(is_goal_check(nodes.back().x, nodes.back().y) == true)
       flag_goal_check_ = true;
@@ -391,7 +464,7 @@ void AdTargetPathPlanner::process()
 
   while(ros::ok())
   {
-    if((flag_cost_map_ == true) && (flag_local_goal_ == true))
+    if((flag_cost_map_ == true) && (flag_local_goal_ == true) && (flag_glocal_path_ == true))
     {
       create_path(max_rad);
     }
@@ -399,6 +472,7 @@ void AdTargetPathPlanner::process()
     // msgの受け取り判定用flagをfalseに戻す
     flag_cost_map_ = false;
     flag_local_goal_ = false;
+    flag_glocal_path_ = false;
 
     ros::spinOnce();
     loop_rate.sleep();
